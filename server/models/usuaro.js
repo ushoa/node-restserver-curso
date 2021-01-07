664b19187423969147e9cb10db50eaf67ad02dd6
Ivan Usheff <iusheff@gmail.com>
2021-01-07 18:17:15 -0300

    guardado, editado, filtrar con limites el get

--- server/models/usuaro.js
+++ server/models/usuaro.js
@@ -1,26 +1,35 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let roles = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} NO ES UN ROLE VALIDO'
+}
+
 let Schema = mongoose.Schema;
 
 let usuarioSchema = new Schema({
     nombre: {
         type: String,
-        required: [true,'El nombre es necesario']
+        required: [true, 'El nombre es necesario']
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     pass: {
         type: String,
-        required: [true, 'La contraseña es obligatoria']
+        required: [true, 'La contraseï¿½a es obligatoria']
     },
     img: {
         type: String,
-        required:false
+        required: false
     },
     role: {
         type: String,
-        default:"USER_ROLE"
+        default: "USER_ROLE",
+        enum: roles
     },
     estado: {
         type: Boolean,
@@ -28,8 +37,18 @@ let usuarioSchema = new Schema({
     },
     google: {
         type: Boolean,
-        default:false
+        default: false
     }
 });
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.pass;
+
+    return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
 
 module.exports = mongoose.model('usuario', usuarioSchema);
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/usuaro');
 
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario LOCAL!!!');
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let hasta = req.query.hasta || 5;
+    hasta = Number(hasta);
+    Usuario.find({})
+        .skip(desde)
+        .limit(hasta)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    conteo
+                });
+            })
+        });
 });
 
-app.post('/usuario', function (req, res) {
-
+app.post('/usuario', function(req, res) {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        pass: body.pass,
+        pass: bcrypt.hashSync(body.pass, 10),
         role: body.role
     });
     usuario.save((err, usuarioDB) => {
@@ -22,6 +44,7 @@ app.post('/usuario', function (req, res) {
                 err
             });
         }
+        //usuario.pass = null;
         res.json({
             ok: true,
             usuario: usuarioDB
@@ -30,16 +53,24 @@ app.post('/usuario', function (req, res) {
     });
 });
 
-app.put('/usuario/:id', function (req, res) {
-
+app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-
-    res.json({
-        id
-    });
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            user: usuarioDB
+        });
+    })
 });
 
-app.delete('/usuario', function (req, res) {
+app.delete('/usuario', function(req, res) {
     res.json('delete Usuario');
 });
 
