c1dd0984d432418a3fb5b458cc4320aec615c6b4
Ushoa <Ushoa@DESKTOP-S78RADK>
2021-01-10 15:18:12 -0300

    login

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,9 +4,36 @@ const Usuario = require('../models/usuaro');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
+    let body=req.body
+    Usuario.findOne({email:body.email},(err,usuarioDB)={
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioDB){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message:'Usuario y contraseña no coinciden'
+                }
+            });
+        }
+        if(!bcrypt.compareSync(body.pass,usuarioDB.pass)){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message:'Usuario y contraseña no coinciden'
+                }
+            });
+        }
+        res.json({
+            ok:true,
+            usuarioDB,
+            token:'123'
+        });
     });
-})
+});
 
 module.exports = app;
