08f9787a089052ab241f26d133e1995342a2d242
Ivan Usheff <iusheff@gmail.com>
2021-01-11 11:14:08 -0300

    login

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,34 +4,34 @@ const Usuario = require('../models/usuaro');
 const app = express();
 
 app.post('/login', (req, res) => {
-    let body=req.body
-    Usuario.findOne({email:body.email},(err,usuarioDB)=>{
+    let body = req.body
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
                 err
             });
         }
-        if (!usuarioDB){
+        if (!usuarioDB) {
             return res.status(400).json({
                 ok: false,
-                err:{
-                    message:'Usuario y contraseña no coinciden'
+                err: {
+                    message: '(Usuario) y contraseña no coinciden'
                 }
             });
         }
-        if(!bcrypt.compareSync(body.pass,usuarioDB.pass)){
+        if (!bcrypt.compareSync(body.pass, usuarioDB.pass)) {
             return res.status(400).json({
                 ok: false,
-                err:{
-                    message:'Usuario y contraseña no coinciden'
+                err: {
+                    message: 'Usuario y (contraseña) no coinciden'
                 }
             });
         }
         res.json({
-            ok:true,
+            ok: true,
             usuarioDB,
-            token:'123'
+            token: '123'
         });
     });
 });
